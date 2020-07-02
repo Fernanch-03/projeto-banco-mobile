254e4be790f96589951a1f96ad6f6f4a61fb72ab
Fernanch-03 <fefemachado03@gmail.com>
2020-07-03 08:05:24 +0900

    Adicionado funções ao listar, porém os html não se conectam ao js, e eu não faço ideia do porque

--- www/lib/banco.js
+++ www/lib/banco.js
@@ -5,7 +5,7 @@ $(document).on("click","#listar", function(){
 });
 //ações de banco
 $(document).on("click", "#salvar",function(){
-  var paramentros ={
+  var parametros ={
   "nome":$("#nome").val(),
   "email":$("#email").val(),
   "senha":$("#senha").val()
@@ -14,7 +14,7 @@ $(document).on("click", "#salvar",function(){
   $.ajax({
     type:"post",//metodo
     url:"https://wordpress-online-2.000webhostapp.com/webservice/cadastra.php",//para onde
-    data:parametros,//o que enviar
+    data: parametros,//o que enviar
     //se der certo
     success:function(data){
       navigator.notification.alert(data);
@@ -32,17 +32,99 @@ $(document).on("click", "#salvar",function(){
 function listar(){
   $.ajax({
     type:"post",
-    url:"https://wordpress-online-2.000webhostapp.com/webservice/cadastra.php",
+    url:"https://wordpress-online-2.000webhostapp.com/webservice/listar.php",
     datatype:"json",//o que vou receber ou comoi vou receber
     success: function(data){
-      var itemista = "";
+      var itemlista = "";
       $.each(data.pessoas, function(i,dados){
         itemlista += "<option value="+dados.codigo+">"+dados.nome+"</option>";
       });
-      $("#listarPessoas").html(itemista);
+      $("#listaPessoas").html(itemlista);
     },
     error: function(data){
       navigator.notification.alert("Erro no cadastro");
     }
   });
 }
+
+$(document).on("change", "listaPessoas", function(){
+  var parametros = {
+    "codigo" : $("optionselected", ("#listaPessoas")).val() 
+  }
+
+  $.ajax({
+    type:"post",
+    url:"https://wordpress-online-2.000webhostapp.com/webservice/listar-um-registro.php",
+    data: parametros,
+    datatype:"json",//o que vou receber ou comoi vou receber
+    success: function(data){
+      $("#codigo").val(data.pessoa.codigo);
+      $("#nome").val(data.pessoa.nome);
+      $("#email").val(data.pessoa.email);
+      $("#senha").val(data.pessoa.senha);
+    },
+    error: function(data){
+      navigator.notification.alert("Erro no cadastro");
+    }
+  });
+});
+
+function liberarCampos(){
+  $("#nome").prop("readonly",false);
+  $("#email").prop("readonly",false);    $("#senha").prop("readonly",false);
+}
+function desabilitarCampos(){
+  $("#nome").prop("readonly",true);
+  $("#email").prop("readonly",true);    
+  $("#senha").prop("readonly",true);
+}
+
+$(docment).on("click","#editar",function(){
+  liberarCampos();
+});
+
+$(docment).on("click","#cancelar",function(){
+  desabilitarCampos();
+});
+
+
+$(docment).on("click","#salvarEdit",function(){
+  var parametros ={
+  "codigo":$("#codigo").val(),
+  "nome":$("#nome").val(),
+  "email":$("#email").val(),
+  "senha":$("#senha").val()
+  }
+  $.ajax({
+    type:"post",
+    url:"https://wordpress-online-2.000webhostapp.com/webservice/atualiza.php",
+    data: parametros,
+    success:function(data){
+      navigator.notification.alert(data);
+      location.reload();
+    },
+    // der errado
+    error: function(data){
+      navigator.notification.alert("Erro no cadastro");
+    }
+  });
+});
+
+$(docment).on("click","#excluir",function(){
+  var parametros ={
+  "codigo":$("#codigo").val(),
+  }
+  $.ajax({
+    type:"post",
+    url:"https://wordpress-online-2.000webhostapp.com/webservice/delete.php",
+    data: parametros,
+    success:function(data){
+      navigator.notification.alert(data);
+      location.reload();
+      desabilitarCampos();
+    },
+    error: function(data){
+      navigator.notification.alert("Erro no cadastro");
+    }
+  });
+})
